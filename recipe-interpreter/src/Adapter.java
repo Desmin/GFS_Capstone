9481201e622571ba8b41f0637bff690a81908a53
Wesley Guthrie <guthriew@arch03.cis.gvsu.edu>
2018-02-13 18:51:21 -0500

    Created adapter skeleton code

---
+++ recipe-interpreter/src/Adapter.java
@@ -0,0 +1,34 @@
+public class Adapter {
+
+    public Adapter() {
+
+    }
+
+    public boolean connect() {
+        return false;
+    }
+
+    public boolean disconnect() {
+        return false;
+    }
+
+    public boolean sendImage() {
+        return false;
+    }
+
+    public boolean sendOptions() {
+        return false;
+    }
+
+    public boolean getText() {
+        return false;
+    }
+
+    public boolean processImage() {
+        return false;
+    }
+
+    public boolean convertImage() {
+        return false;
+    }
+}
